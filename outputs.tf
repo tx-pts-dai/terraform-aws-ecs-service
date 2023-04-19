b10edc24ad6a05a8c4d461c675eead0913276388
Roland Bapst <roland.bapst@tx.group>
2023-04-19 12:07:13 +0200

    add iam_role_name output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "iam_role_arn" {
   description = "ARN of the created ECS IAM role used to execute the task"
   value       = aws_iam_role.this.arn
 }
+
+output "iam_role_name" {
+  description = "Name of the created ECS IAM role used to execute the task"
+  value       = aws_iam_role.this.name
+}
