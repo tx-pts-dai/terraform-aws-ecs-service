34d04faee70a0fc67bbf615fd2993a10915ebb03
Christian Jürges <36044481+chrisamti@users.noreply.github.com>
2026-05-20 15:03:05 +0200

    FUM-4116: replace aws_region.current.name with aws_region.current.region (#18)
    The `name` attribute of the `aws_region` data source is deprecated in
    favor of `region` as of AWS provider v6.0.0. Bump the minimum provider
    version to >= 6.0 accordingly.

--- ecs_service.tf
+++ ecs_service.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_task_definition" "this" {
         logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.this.name
-          awslogs-region        = data.aws_region.current.name
+          awslogs-region        = data.aws_region.current.region
           awslogs-stream-prefix = "ecs"
         }
       }
